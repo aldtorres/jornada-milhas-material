7ae356925755ec9355ecefd54d2bcc5672f5b655
torrestor <torres.jovem@gmail.com>
2026-01-20 14:53:28 -0300

    componente banner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { HeaderComponent } from "./components/header/header.component";
+import { BannerComponent } from "./components/banner/banner.component";
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule, HeaderComponent],
+  imports: [MatButtonModule, HeaderComponent, BannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
