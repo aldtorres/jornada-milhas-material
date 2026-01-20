79d745ab077e4486e80d261ea39a0bf4b42b25ea
torrestor <torres.jovem@gmail.com>
2026-01-20 16:37:28 -0300

    MatButtonToggleModule / MatFormFieldModule / MatInputModule / MatIconModule / MatButtonModule / MatTooltipModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { HeaderComponent } from "./components/header/header.component";
 import { BannerComponent } from "./components/banner/banner.component";
+import { FormBuscaPassagensComponent } from "./components/form-busca-passagens/form-busca-passagens.component";
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule, HeaderComponent, BannerComponent],
+  imports: [MatButtonModule, HeaderComponent, BannerComponent, FormBuscaPassagensComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
